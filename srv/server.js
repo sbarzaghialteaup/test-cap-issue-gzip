10fb5e1d270e3be017d05d71bb4e57f5594160a7
Samuele Barzaghi <sbarzaghi@alteanet.it>
2022-07-07 16:38:55 +0200

    Issue setting JSON limit

--- srv/server.js
+++ srv/server.js
@@ -1,6 +1,7 @@
 const cds = require("@sap/cds");
 
 cds.on("bootstrap", (app) => {
+  app.use(require("express").json({ limit: "10MB" }));
   cds.serve("CatalogService").in(app).to("rest").at("rest");
 });
 
